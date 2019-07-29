9475622f9225af8111ef93fb5cd5d23a923f3743
Nubelson <therealnletter@hotmail.com>
2019-07-29 01:43:35 +0100

    reat time config

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -4,6 +4,9 @@ import api from '../../services/api';
 import { distanceInWords } from 'date-fns';
 import en from 'date-fns/locale/en';
 
+import Dropzone from 'react-dropzone';
+import socket from 'socket.io-client';
+
 import './styles.css';
 import logo2 from '../../assets/logo2.svg';
 
@@ -13,11 +16,37 @@ export default class Box extends Component {
 	};
 
 	async componentDidMount() {
+		this.subscribeToNewFiles();
+
 		const box = this.props.match.params.id;
 		const response = await api.get(`box/${box}`);
 
 		this.setState({ box: response.data });
 	}
+
+	subscribeToNewFiles = () => {
+		const box = this.props.match.params.id;
+		const io = socket('https://dropboxclonethenletter.herokuapp.com');
+
+		io.emit('connectRoom', box);
+
+		io.on('file', data => {
+			this.setState({
+				box: { ...this.state.box, files: [data, ...this.state.box.files] }
+			});
+		});
+	};
+
+	handleUpload = files => {
+		files.forEach(file => {
+			const data = new FormData();
+			const box = this.props.match.params.id;
+
+			data.append('file', file);
+
+			api.post(`box/${box}/files`, data);
+		});
+	};
 
 	render() {
 		return (
@@ -27,10 +56,20 @@ export default class Box extends Component {
 					<h1>{this.state.box.title}</h1>
 				</header>
 
+				<Dropzone onDropAccepted={this.handleUpload}>
+					{({ getRootProps, getInputProps }) => (
+						<div className="upload" {...getRootProps()}>
+							<input {...getInputProps()} />
+
+							<p>Drag files or click here</p>
+						</div>
+					)}
+				</Dropzone>
+
 				<ul>
 					{this.state.box.files &&
 						this.state.box.files.map(file => (
-							<li>
+							<li key={file._id}>
 								<a
 									className="fileInfo"
 									href={file.url}
